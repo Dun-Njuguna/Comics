6e2aa603a9ba856110d9fd3734b6a98844ac3d41
Dun-Njuguna <duncankariuki60@gmail.com>
2019-03-01 14:42:34 +0300

    modified dependencies

--- app/src/main/java/com/example/movies/MoviesActivity.java
+++ app/src/main/java/com/example/movies/MoviesActivity.java
@@ -3,12 +3,25 @@ package com.example.movies;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MoviesActivity extends AppCompatActivity {
+
+//    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+//            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+//            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+//            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+//            "Chipotle", "Subway"};
+
+    private ListView mListView;
 
     @BindView(R.id.searchTextView) TextView mSearchTextView;
 
@@ -17,6 +30,18 @@ public class MoviesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
         ButterKnife.bind(this);
+        mListView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
+
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String restaurant = ((TextView)view).getText().toString();
+//                Toast.makeText(MoviesActivity.this, restaurant, Toast.LENGTH_LONG).show();
+//            }
+//        });
 
         Intent intent = getIntent();
         String search = intent.getStringExtra("text");
