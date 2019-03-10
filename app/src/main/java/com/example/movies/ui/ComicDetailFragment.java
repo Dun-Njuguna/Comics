24d891426e4a6ecbb3f0cdcb8cbab2352dc8c0c5
Dun-Njuguna <duncankariuki60@gmail.com>
2019-03-10 22:23:31 +0300

    included a custom fragment

---
+++ app/src/main/java/com/example/movies/ui/ComicDetailFragment.java
@@ -0,0 +1,30 @@
+package com.example.movies.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.movies.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ComicDetailFragment extends Fragment {
+
+
+    public ComicDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_comic_detail, container, false);
+    }
+
+}
