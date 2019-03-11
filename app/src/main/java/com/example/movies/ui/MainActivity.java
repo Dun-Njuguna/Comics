a3c890a01fac561a38222af22b1734a550291f0c
Dun-Njuguna <duncankariuki60@gmail.com>
2019-03-11 10:36:30 +0300

    implemented carousel view

--- app/src/main/java/com/example/movies/ui/MainActivity.java
+++ app/src/main/java/com/example/movies/ui/MainActivity.java
@@ -6,8 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.movies.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,6 +20,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.search) Button search;
     @BindView(R.id.searchtext) EditText title;
 
+    CarouselView carouselView;
+
+    int[] sampleImages = {R.drawable.image9, R.drawable.ml, R.drawable.images12};
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +33,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ButterKnife.bind(this);
         search.setOnClickListener(this);
 
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+
+        carouselView.setImageListener(imageListener);
+
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 
     @Override
     public void onClick(View v) {
