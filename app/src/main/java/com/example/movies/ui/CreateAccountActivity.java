104a231319536d55ec90a9830c693a32d136b41d
Dun-Njuguna <duncankariuki60@gmail.com>
2019-03-18 02:28:35 +0300

    created firebase auth state listener

--- app/src/main/java/com/example/movies/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/movies/ui/CreateAccountActivity.java
@@ -82,5 +82,22 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                 });
     }
 
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+    }
+
 
 }
